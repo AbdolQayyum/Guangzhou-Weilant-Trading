7494e62e3d518f5d53b31fe7eac3bbff61779123
Mac <mac@Macs-MBP-5.lan>
2026-02-20 12:49:45 +0500

    update title

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/Navbar";
@@ -10,12 +10,27 @@ import Process from "./pages/Process";
 import Contact from "./pages/Contact";
 import About from "./pages/About";
 import ExploreServices from "./pages/ExploreServices";
-import { LanguageProvider } from "./LanguageContext";
+import { LanguageProvider, useLanguage } from "./LanguageContext";
+
+// Component to update document title based on language
+function TitleUpdater() {
+  const { language } = useLanguage();
+
+  useEffect(() => {
+    const title = language === "zh" 
+      ? "广州维朗贸易有限公司" 
+      : "Guangzhou Weilant Trading Co Ltd";
+    document.title = title;
+  }, [language]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
       <LanguageProvider>
+        <TitleUpdater />
         <Navbar />
 
         <Routes>
